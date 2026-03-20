e433757bd47a0448638375aa4f0512d2113b556a
kihyren <obinnaezejoshua@gmail.com>
2026-03-20 09:49:22 -0600

    Imported classes and functions from the different modules from earlier branches for use later.

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-# Main app that runs the final project
+# Importing classes and functions from different modules
 from llm_deadline_extraction import extract_deadlines
+from dataframe_processing import DataFrameEditor
+from ui import FileUploadUI
+from file_parser import parse_files
+from excel_export import export_to_excel
+import tkinter as tk
